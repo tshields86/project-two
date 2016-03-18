8bbbf79563ce173c83ca3a2ab082367476a50090
Travis Shields <travis.shields@gmail.com>
2016-03-18 13:33:34 -0400

    Struggled through CORS issues and got the first API to work via jsonp. Took the data I needed from the first ajax call and used the song id to run the socond ajax call to obtain lyrics. used .slice to take off the footer of the lyrics and am now working on inputting it into the yoda ajax call.

--- index.js
+++ index.js
@@ -1,31 +1,86 @@
+document.addEventListener("DOMContentLoaded", function(event){
+
+
+
+$('#submit-btn').on('click', function() {
+var artist = document.getElementById('artist-search').value;
+if (artist === ''){
+  alert("Please provide an artist.");
+  return;
+}
+var song = document.getElementById('song-search').value;
+if (song === ''){
+  alert("Please provide a song.");
+  return;
+}
+
+var songQuery = 'http://api.musixmatch.com/ws/1.1/track.search?format=jsonp&q_track=' + song + '&q_artist=' + artist + '&f_has_lyrics=1&apikey=' + musixMatchKey;
 
 $.ajax({
-  url: "https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday",
+  url: songQuery,
+  dataType: 'jsonp',
+  jsonpCallback: "callback"
+}).done(function(data) {
+    // console.log("This is the full data: ",data);
+  // console.log(data.message.body.track_list[0]);
+  info.trackId = data.message.body.track_list[0].track.track_id;
+  info.artistName = data.message.body.track_list[0].track.artist_name;
+  info.albumName = data.message.body.track_list[0].track.album_name;
+  info.trackName = data.message.body.track_list[0].track.track_name;
+  info.albumCover = data.message.body.track_list[0].track.album_coverart_350x350;
+  // console.log("Info: ",info);
 
-  // type: 'POST',
-  // data: {},
-  // dataType: 'json',
+  var  lyricQuery = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&track_id=' + info.trackId + '&apikey=' + musixMatchKey;
+  // console.log(lyricQuery);
+  $.ajax({
+    url: lyricQuery,
+    dataType: 'jsonp',
+    jsonpCallback: "callback"
+  }).done(function(data) {
+    // console.log("Lyrics search data : ",data);
+    info.trackLyrics = data.message.body.lyrics.lyrics_body;
+    console.log(info);
+    // console.log(info.trackLyrics);
+    // console.log(typeof info.trackLyrics);
+    info.trackLyrics = info.trackLyrics.slice(0, -58)
+    console.log(info.trackLyrics);
+    var yodaQuery = "https://yoda.p.mashape.com/yoda?sentence=";
+        // example = "You will learn how to speak like me someday";
 
-  beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", 'lHg2Psw6tLmshsI058Xhf6EickOZp1g2Juojsn0KNtkjMllIxC');
-  },
-  success: function(data) {
-    console.log(data);
-  },
-  error: function(err) {
-    console.log("error");
-  }
+    $.ajax({
+      url: yodaQuery + info.trackLyrics,
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(err) {
+        console.log("error");
+      },
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("X-Mashape-Authorization", yodaKey);
+      }
+      // used beforeSend to input the X-Mashape-Authorization key
+    });//end of yoda ajax
+  });//end of lyricQuery ajax
 
 
-// }).done(function(response){
-//       console.log(response);
-//
-//
-//
-//
-//     }).fail(function(response){
-//       console.log("fail");
-//       console.log(response);
-//     }).always(function(response){
-//       console.log("this runs no matter what.");
-    });
+
+
+
+
+});//end of songQuery ajax
+
+
+});//end of submit click function
+
+var info = {
+  trackId: undefined,
+  artistName: undefined,
+  albumName: undefined,
+  trackName: undefined,
+  albumCover: undefined,
+  trackLyrics: undefined
+};
+
+
+
+});//end of dom content loaded
